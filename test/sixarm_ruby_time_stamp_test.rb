37477168ee4487151df31e01a16d34f14ae60164
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-08 13:27:01 -0600

    Upgrade from MiniTest to Minitest

--- test/sixarm_ruby_time_stamp_test.rb
+++ test/sixarm_ruby_time_stamp_test.rb
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 require "minitest/autorun"
-Minitest::Test ||= MiniTest::Unit::TestCase
 require "simplecov"
 SimpleCov.start
 require "sixarm_ruby_time_stamp"
@@ -16,8 +15,7 @@ describe Time do
     it "with instance method" do
       Time.now.stamp.must_match /^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\dZ$/
     end
- 
+
   end
 
 end
-
